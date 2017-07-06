50cd4412798a9822fa69663e9d55ff157b96c33f
Sumit Roy <sroy8091@gmail.com>
2017-07-06 16:16:22 +0530

    html files

--- app/src/server.js
+++ app/src/server.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var app = express();
+var root = process.cwd();
 
 //your routes here
 app.get('/', function (req, res) {
     res.send("Hello World!");
 });
+
+app.get('/login', function (req, res) {
+    res.sendFile("html/login.html", {root});
+});
+
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
