7bc0fbd1f83b594cc550c4fb8234a67eab160cbd
Sumit Roy <sroy8091@gmail.com>
2017-07-23 14:43:34 +0530

    basic setup with view engine

--- app/src/server.js
+++ app/src/server.js
@@ -1,17 +1,123 @@
-var express = require('express');
-var app = express();
-var root = process.cwd();
+const express = require('express');
+const path = require('path');
+const fetch = require('node-fetch');
+const app = express();
 
-//your routes here
+//setting static files location
+app.use(express.static(path.join(__dirname, 'static')));
+
+//setting the view engine
+app.set("view engine", 'ejs');
+
+
+// A nice landing page is to be added.
+//finding a way to render css and js files in node--done
+//login and signup using hausura auth api--done
+//adding a user in hasura db and inserting a snipcode using hasura data api
+
+
+/* body parser is used for handling post request.
+Previous versions of express includes this but from version 4 it comes as an external package */
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}));
+
+/* root defining is required to sendFile in routes and addressing
+them from root project directory.
+In this case we are defining the root as /home/sroy/snipcode/app/src i.e current directory.*/
+const root = process.cwd();
+
+//home page route
 app.get('/', function (req, res) {
-    res.send("Hello World!");
+    res.render("index.ejs");
 });
 
+// login page route
 app.get('/login', function (req, res) {
-    res.sendFile("html/login.html", {root});
+	res.render("login");
+});
+
+//post request for login using hasura auth api
+app.post('/login', function(req, res) {
+	const schemaFetchUrl = 'http://auth.c100.hasura.me/login';
+	const options = {
+	  method: 'POST',
+      headers: {
+	      'Content-Type':'application/json'
+      },
+        body: JSON.stringify({
+            username: req.body.username,
+            password: req.body.password
+        })
+    };
+	fetch(schemaFetchUrl, options)
+        .then(function (res) {
+            console.log('status: '+res.status);
+            return res.json();
+        }).then(function (json) {
+        console.log(json);
+        headers.Authorization = 'Bearer'+ json['auth_token'];
+        headers['X-Hasura-User-Id'] = json['hasura_id'];
+        headers['X-Hasura-Role'] = json['hasura_roles'][0];
+        console.log(headers);
+    });
+	res.redirect('/login');
+	console.log();
+});
+
+//register/signup route
+app.get('/register', function(req, res) {
+	res.render("register");
+});
+
+//post route for register/signup
+app.post('/register', function (req, res) {
+    const schemaFetchUrl = 'http://auth.c100.hasura.me/signup';
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username: req.body.username,
+            password: req.body.password
+            //wil add this after email verification in hasura console
+            // email: req.body.email
+        })
+    };
+    fetch(schemaFetchUrl, options)
+        .then(function(res) {
+            return res.json();
+        }).then(function(json) {
+        console.log(json);
+        //
+        headers.Authorization = 'Bearer ' + json['auth_token'];
+        const options2 = {
+            method: 'POST',
+            headers,
+            body: JSON.stringify({
+                type: 'insert',
+                args: {
+                    table: 'profile',
+                    objects: [{
+                        'name': newuser['username'],
+                        'user_id': json['hasura_id']
+                    }]
+                }
+            })
+        };
+        fetch('http://data.c100.hasura.me/', options2)
+            .then(function(res2) {
+                return res2.json();
+            }).then(function(json2) {
+            console.log(json2);
+        })
+    });
+    res.redirect("/");
+    console.log();
 });
 
 
+//server starts here
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+  console.log('Example app listening on port 8080! '+root);
 });
---
+++ app/src/static/js/index.js
@@ -0,0 +1,35 @@
+$(function(){
+	$(".typed").typed({
+		strings: ["Save", "Share"],
+		// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+		stringsElement: null,
+		// typing speed
+		typeSpeed: 100,
+		// time before typing starts
+		startDelay: 1200,
+		// backspacing speed
+		backSpeed: 50,
+		// time before backspacing
+		backDelay: 800,
+		// loop
+		loop: true,
+		// show cursor
+		showCursor: false,
+		// character for cursor
+		cursorChar: "|",
+		// attribute to type (null == text)
+		attr: null,
+		// either views or text
+		contentType: 'html',
+		// call when done callback function
+		callback: function() {},
+		// starting callback function before each string
+		preStringTyped: function() {},
+		//callback for every typed string
+		onStringTyped: function() {},
+		// callback for reset
+		resetCallback: function() {}
+	});
+});
+
+$('.dropdown-toggle').dropdown();
