4c6e0d05fd8f408bb82ad5759e90c975b14e50ed
Sumit Roy <sroy8091@gmail.com>
2017-07-25 18:26:35 +0530

    added login register and logout

--- app/src/server.js
+++ app/src/server.js
@@ -1,10 +1,21 @@
+
 /* ---------TODOS----------*/
 
 // A nice landing page is to be added. --done
 //finding a way to render css and js files in node--done
 //login and signup using hausura auth api--done
-// printing logged in user's name.
-// adding all pages ejs.
+// logout using hasura auth api.
+// adding user in user_info table to store more info. --done
+// printing logged in user's name.--done
+// showing errors of login and registration --done
+// dynamic dropdown.
+// segregating header and footer.
+// getting the dashboard page
+// new snip page
+// search page like wikipedia search
+// redirecting logged in user's to dashboard and registered user to new snip
+// edit profile page
+// adding all pages ejs. --done
 //adding a user in hasura db and inserting a snipcode using hasura data api
 
 /* ------------------------ */
@@ -35,58 +46,100 @@ them from root project directory.
 In this case we are defining the root as /home/sroy/snipcode/app/src i.e current directory.*/
 const root = process.cwd();
 
+const authUrl = "http://auth.c100.hasura.me/";
+const dataUrl = "http://data.c100.hasura.me/v1/query";
 
 //home page route
 app.get('/' ,function (req, res) {
-    console.log(req.cookies.userName);
-    const user = req.cookies.userName;
-    res.render("index.ejs", {user: user});
+    // console.log(req.cookies.userName);
+    var user = "guest";
+    const title = "SNIPCODE";
+    console.log(req.cookies);
+    // console.log(req.headers);
+    if (req.cookies.userName!=="")
+        user = req.cookies.userName;
+
+    res.render("index", {user: user, title:title});
 });
 
 // login page route
 app.get('/login', function (req, res) {
-	res.render("login");
+    // console.log(req.cookies);
+    res.render("login", {rstatus: null});
 });
 
 //post request for login using hasura auth api
 app.post('/login', function(req, res) {
-	const authUrl = 'http://auth.c100.hasura.me/login';
+    const url = authUrl+'login';
     const username = req.body.username;
-	const options = {
-	  method: 'POST',
-      headers: {
-	      'Content-Type':'application/json'
-      },
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type':'application/json'
+        },
         body: JSON.stringify({
             username: username,
             password: req.body.password
         })
     };
-	fetch(authUrl, options, {credentials: 'include'})
+
+    fetch(url, options)
         .then(function (res) {
             console.log('status: '+res.status);
             return res.json();
         }).then(function (json) {
-        // console.log(json);
-        res.cookie("userId", json['hasura_id']);
-        res.cookie("userName",username);
-        res.cookie("Authorization" , json['auth_token']);
-        // console.log(res.cookie);
-        res.redirect("/");
+        console.log(json);
+        if (json['message'])
+            res.render("login", {rstatus: json['message']});
+        else{
+            res.cookie("userId", json['hasura_id']);
+            res.cookie("userName",username);
+            res.cookie("Authorization" , json['auth_token']);
+            // console.log(res.cookie);
+            res.redirect("/");
+        }
+
     }).catch(function (err){
-       // console.error(err);
-       res.redirect("/login");
+        console.error(err);
+        res.render("login", {rstatus: "Host Unreachable"});
     });
+    // console.log();
+});
+
+app.get('/logout', function(req, res){
+    const url = authUrl+'user/logout';
+    const options = {
+        method: 'GET',
+        headers: {
+            'Authorization': req.cookies.Authorization
+        }
+    };
+
+    fetch(url, options)
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (json) {
+            res.clearCookie('Authorization');
+            res.clearCookie('userName');
+            res.clearCookie('userId');
+            res.redirect("/login");
+        }).catch(function (err) {
+        console.error(err);
+        res.redirect("/");
+    });
+    // res.redirect("/")
 });
 
 //register/signup route
 app.get('/register' , function(req, res) {
-	res.render("register");
+    res.render("register", {rstatus: null});
 });
 
 //post route for register/signup
 app.post('/register', function (req, res) {
-    const authUrl = 'http://auth.c100.hasura.me/signup';
+    const url = authUrl+'signup';
+    const username = req.body.username;
     const options = {
         method: 'POST',
         headers: {
@@ -99,40 +152,65 @@ app.post('/register', function (req, res) {
             // email: req.body.email
         })
     };
-    fetch(authUrl, options)
+    fetch(url, options, {credentials: 'include'})
         .then(function(res) {
             return res.json();
         }).then(function(json) {
-        console.log(json);
-        //
-        headers.Authorization = 'Bearer ' + json['auth_token'];
-        const options2 = {
-            method: 'POST',
-            headers,
-            body: JSON.stringify({
-                type: 'insert',
-                args: {
-                    table: 'profile',
-                    objects: [{
-                        'name': newuser['username'],
-                        'user_id': json['hasura_id']
-                    }]
-                }
+        // console.log(json);
+        if (json['message'])
+            res.render('register', {rstatus: json['message']});
+        else {
+            res.cookie("userId", json['hasura_id']);
+            res.cookie("userName", username);
+            res.cookie("Authorization", json['auth_token']);
+            const options2 = {
+                method: 'POST',
+                headers: {
+                    "Authorization": "Bearer " + json['auth_token']
+                },
+                body: JSON.stringify({
+                    type: 'insert',
+                    args: {
+                        table: 'user_info',
+                        objects: [{
+                            'name': username,
+                            'user_id': json['hasura_id']
+                        }]
+                    }
+                })
+            };
+            fetch(dataUrl, options2)
+                .then(function(res2) {
+                    return res2.json();
+                }).then(function(json2) {
+                // console.log(json2);
+                res.redirect("/");
             })
-        };
-        fetch('http://data.c100.hasura.me/', options2)
-            .then(function(res2) {
-                return res2.json();
-            }).then(function(json2) {
-            console.log(json2);
-        })
+        }
+
     });
-    res.redirect("/");
-    console.log();
 });
+
+
+function isLoggedIn(req, res, next) {
+
+    // if user is authenticated in the session, carry on
+    if (req.headers.Authorization)
+        return next();
+
+    // if they aren't redirect them to the home page
+    res.redirect('/');
+}
+
+//dashboard route
+app.get('/profile', isLoggedIn, function (req, res) {
+    res.render('dashboard');
+});
+
+//new snip route
 
 
 //server starts here
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080! '+root);
+    console.log('Example app listening on port 8080! '+root);
 });
