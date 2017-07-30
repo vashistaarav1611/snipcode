20ae87538a99556162f41d3320481f3fc6a41db4
Sumit Roy <sroy8091@gmail.com>
2017-07-30 09:24:55 +0530

    search animation

--- app/src/static/js/search.js
+++ app/src/static/js/search.js
@@ -1,9 +1,10 @@
 $(document).ready(function () {
+
     $('#searchForm').submit(function (event) {
         event.preventDefault();
         var searchText = $('#searchText').val();
         // console.log(searchText);
-        $("#results").empty();
+        var delay = 2000;
         $.ajax({
             url: "/search",
             data: JSON.stringify(
@@ -11,19 +12,25 @@ $(document).ready(function () {
             ),
             type: 'POST',
             headers: {'Content-Type': 'application/json'},
+            beforeSend: function() {
+                $('#results').html("<img src='/img/Magnify.svg'/>");
+            },
             success: function (data) {
-                console.log(data[1][5]);
-                if(data.length===1)
-                    $("#results").append('<li><a><h3 class="title ">No Results Found</h3></a><p class="text">Try Another keyword</p></li>');
-                else {
-                    var obj = data;
-                    for (var i = 1; i < obj.length; i++) {
-                        $("#results").append('<a target="_blank" href="/searchroute/' + obj[i][0] + '">' +
-                            '<li><h3 class="title">' + obj[i][1] +
-                            '</h3></a><p class="text">in ' + obj[i][3] + ' by <a href="/profile/'+ obj[i][6] +
-                            '"><i>' + obj[i][5] + '</i></a></p></li>');
+                setTimeout(function() {
+                    $("#results").empty();
+                    if(data.length===1)
+                        $("#results").append('<li><a><h3 class="title ">No Results Found</h3></a><p class="text">Try Another keyword</p></li>');
+                    else {
+                        var obj = data;
+                        for (var i = 1; i < obj.length; i++) {
+                            $("#results").append('<a target="_blank" href="/searchroute/' + obj[i][0] + '">' +
+                                '<li><h3 class="title">' + obj[i][1] +
+                                '</h3></a><p class="text">in ' + obj[i][3] + ' by <a href="/profile/'+ obj[i][6] +
+                                '"><i>' + obj[i][5] + '</i></a></p></li>');
+                        }
                     }
-                }
+                }, delay);
+
             }
         });
     })
